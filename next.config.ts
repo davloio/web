8bb154ff856b0d1ed1e37b597cd4dc4cf579eb47
Loïs Kauffungen <lois.kauffungen@gmail.com>
2025-11-17 17:09:26 +0100

    add docker setup

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   transpilePackages: ['three'],
   turbopack: {},
+  output: 'standalone',
 };
 
 export default nextConfig;
